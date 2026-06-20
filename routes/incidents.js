1b6d67377197d0e8339ac46eacf4eeb6fa0e0efa
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-06-20 00:26:45 -0400

    add query filters for incident routes

--- routes/incidents.js
+++ routes/incidents.js
@@ -5,9 +5,35 @@ import incidents from "../data/incidents.js";
 
 const router = express.Router();
 
-// GET /api/incidents, this route shows all incidents.
+// GET /api/incidents, this route shows and filter incidents like severity, status, or authentication
 router.get("/", (req, res) => {
-  res.json(incidents);
+  const { severity, status, category } = req.query;
+
+  // list by filters
+  let filteredIncidents = incidents;
+
+  // Filter by severity if the user adds ?severity=High to the URL.
+  if (severity) {
+    filteredIncidents = filteredIncidents.filter(
+      (incident) => incident.severity.toLowerCase() === severity.toLowerCase()
+    );
+  }
+
+  // Filter by status if the user adds ?status=Open to the URL.
+  if (status) {
+    filteredIncidents = filteredIncidents.filter(
+      (incident) => incident.status.toLowerCase() === status.toLowerCase()
+    );
+  }
+
+  // Filter by category if the user adds ?category=Authentication to the URL.
+  if (category) {
+    filteredIncidents = filteredIncidents.filter(
+      (incident) => incident.category.toLowerCase() === category.toLowerCase()
+    );
+  }
+
+  res.json(filteredIncidents);
 });
 
 // GET /api/incidents/:id, this route shows incidents based on the ID in the URL.
